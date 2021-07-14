8d1623dc323cbe2eb6ec643f079327bd590fa81f
satindergrewal <grewal@lavabit.com>
2021-07-15 03:54:43 +1200

    pirate chain params update

--- zcash_primitives/src/consensus.rs
+++ zcash_primitives/src/consensus.rs
@@ -172,13 +172,13 @@ pub trait Parameters: Clone {
     /// applies.
     ///
     /// [`TransparentAddress::PublicKey`]: zcash_primitives::legacy::TransparentAddress::PublicKey
-    fn b58_pubkey_address_prefix(&self) -> [u8; 2];
+    fn b58_pubkey_address_prefix(&self) -> [u8; 1];
 
     /// Returns the human-readable prefix for Base58Check-encoded transparent pay-to-script-hash
     /// payment addresses for the network to which this Parameters value applies.
     ///
     /// [`TransparentAddress::Script`]: zcash_primitives::legacy::TransparentAddress::Script
-    fn b58_script_address_prefix(&self) -> [u8; 2];
+    fn b58_script_address_prefix(&self) -> [u8; 1];
 }
 
 /// Marker struct for the production network.
@@ -190,11 +190,11 @@ pub const MAIN_NETWORK: MainNetwork = MainNetwork;
 impl Parameters for MainNetwork {
     fn activation_height(&self, nu: NetworkUpgrade) -> Option<BlockHeight> {
         match nu {
-            NetworkUpgrade::Overwinter => Some(BlockHeight(347_500)),
-            NetworkUpgrade::Sapling => Some(BlockHeight(419_200)),
-            NetworkUpgrade::Blossom => Some(BlockHeight(653_600)),
-            NetworkUpgrade::Heartwood => Some(BlockHeight(903_000)),
-            NetworkUpgrade::Canopy => Some(BlockHeight(1_046_400)),
+            NetworkUpgrade::Overwinter => Some(BlockHeight(0)),
+            NetworkUpgrade::Sapling => Some(BlockHeight(200_000)),
+            NetworkUpgrade::Blossom => Some(BlockHeight(4_000_000_000)),
+            NetworkUpgrade::Heartwood => Some(BlockHeight(4_000_000_000)),
+            NetworkUpgrade::Canopy => Some(BlockHeight(4_000_000_000)),
             #[cfg(feature = "zfuture")]
             NetworkUpgrade::ZFuture => None,
         }
@@ -216,11 +216,11 @@ impl Parameters for MainNetwork {
         constants::mainnet::HRP_SAPLING_PAYMENT_ADDRESS
     }
 
-    fn b58_pubkey_address_prefix(&self) -> [u8; 2] {
+    fn b58_pubkey_address_prefix(&self) -> [u8; 1] {
         constants::mainnet::B58_PUBKEY_ADDRESS_PREFIX
     }
 
-    fn b58_script_address_prefix(&self) -> [u8; 2] {
+    fn b58_script_address_prefix(&self) -> [u8; 1] {
         constants::mainnet::B58_SCRIPT_ADDRESS_PREFIX
     }
 }
@@ -234,11 +234,11 @@ pub const TEST_NETWORK: TestNetwork = TestNetwork;
 impl Parameters for TestNetwork {
     fn activation_height(&self, nu: NetworkUpgrade) -> Option<BlockHeight> {
         match nu {
-            NetworkUpgrade::Overwinter => Some(BlockHeight(207_500)),
-            NetworkUpgrade::Sapling => Some(BlockHeight(280_000)),
-            NetworkUpgrade::Blossom => Some(BlockHeight(584_000)),
-            NetworkUpgrade::Heartwood => Some(BlockHeight(903_800)),
-            NetworkUpgrade::Canopy => Some(BlockHeight(1_028_500)),
+            NetworkUpgrade::Overwinter => Some(BlockHeight(0)),
+            NetworkUpgrade::Sapling => Some(BlockHeight(200_000)),
+            NetworkUpgrade::Blossom => Some(BlockHeight(4_000_000_000)),
+            NetworkUpgrade::Heartwood => Some(BlockHeight(4_000_000_000)),
+            NetworkUpgrade::Canopy => Some(BlockHeight(4_000_000_000)),
             #[cfg(feature = "zfuture")]
             NetworkUpgrade::ZFuture => None,
         }
@@ -260,11 +260,11 @@ impl Parameters for TestNetwork {
         constants::testnet::HRP_SAPLING_PAYMENT_ADDRESS
     }
 
-    fn b58_pubkey_address_prefix(&self) -> [u8; 2] {
+    fn b58_pubkey_address_prefix(&self) -> [u8; 1] {
         constants::testnet::B58_PUBKEY_ADDRESS_PREFIX
     }
 
-    fn b58_script_address_prefix(&self) -> [u8; 2] {
+    fn b58_script_address_prefix(&self) -> [u8; 1] {
         constants::testnet::B58_SCRIPT_ADDRESS_PREFIX
     }
 }
@@ -311,14 +311,14 @@ impl Parameters for Network {
         }
     }
 
-    fn b58_pubkey_address_prefix(&self) -> [u8; 2] {
+    fn b58_pubkey_address_prefix(&self) -> [u8; 1] {
         match self {
             Network::MainNetwork => MAIN_NETWORK.b58_pubkey_address_prefix(),
             Network::TestNetwork => TEST_NETWORK.b58_pubkey_address_prefix(),
         }
     }
 
-    fn b58_script_address_prefix(&self) -> [u8; 2] {
+    fn b58_script_address_prefix(&self) -> [u8; 1] {
         match self {
             Network::MainNetwork => MAIN_NETWORK.b58_script_address_prefix(),
             Network::TestNetwork => TEST_NETWORK.b58_script_address_prefix(),
